d4249383b6a39e7536810760cf0bd8959862d2b4
Santambrogio Lorenzo <santambrogio.lorenzo@osdbsesto>
2022-02-23 09:06:35 +0100

    init repo

---
+++ ServerSocket01/src/serversocket01/ServerSocket01.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serversocket01;
+
+/**
+ *
+ * @author santambrogio.lorenzo
+ */
+public class ServerSocket01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
