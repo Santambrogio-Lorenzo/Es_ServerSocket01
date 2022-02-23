3ae443535d939aafa3095daad7b0e59fbf901999
Santambrogio Lorenzo <santambrogio.lorenzo@osdbsesto>
2022-02-23 09:36:27 +0100

    add program from github

--- ClientSocket01/src/clientsocket01/ClientSocket01.java
+++ ClientSocket01/src/clientsocket01/ClientSocket01.java
@@ -4,7 +4,11 @@
  * and open the template in the editor.
  */
 package clientsocket01;
-
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 /**
  *
  * @author santambrogio.lorenzo
@@ -14,9 +18,30 @@ public class ClientSocket01 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-    
-        
-    }
-    
+     public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("10.1.33.200", 5000);
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner tastiera = new Scanner(System.in);
+            boolean fine = false;
+            String messaggio = null;
+            while (!fine) {
+                System.out.println("Cosa vuoi scrivere al server?");
+                messaggio = tastiera.next();
+                if (messaggio.equals("fine")) {
+                    fine = true;
+                }
+                out.println(messaggio);
+                System.out.println("Il server risponde: " + in.readLine());
+            }
+            in.close();
+            out.close();
+            clientSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientSocket01.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }      
 }
+    
+
--- ServerSocket01/src/serversocket01/ServerSocket01.java
+++ ServerSocket01/src/serversocket01/ServerSocket01.java
@@ -5,7 +5,10 @@
  */
 package serversocket01;
 
-import javax.xml.crypto.dsig.SignedInfo;
+import java.net.*;
+import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,7 +20,27 @@ public class ServerSocket01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     
+      try {
+            ServerSocket serverSocket = new ServerSocket(5000);
+            Socket clientSocket = serverSocket.accept();
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            boolean fine = false;
+            String messaggio;
+            while (!fine) {
+                messaggio = in.readLine();
+                if (messaggio.equals("fine")) {
+                    fine = true;
+                }
+                out.println(messaggio.toUpperCase());
+            }            
+            in.close();
+            out.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ServerSocket01.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
